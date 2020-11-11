c8f176c63b2651b3eeb1238327079b2c80b4274b
Prateek Kher <prateek.kher@gmail.com>
2020-11-11 13:13:20 +0530

    Wrote Skeleton SignIn

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { 
+    FormBuilder, 
+    FormGroup, 
+    FormControl, 
+    Validators 
+} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +12,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
+    loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+      this.loginForm = this.formBuilder.group({
+          email: ['', [
+              Validators.required, Validators.email
+          ]],
+          password: ['', [
+              Validators.required, 
+              Validators.minLength(6)
+          ]
+      });
+  }
+
+  onSubmit() {
+      if (this.loginform.invalid) {
+          return;
+      }
+
+      console.log(this.loginForm.value);
+  }
+
+  get email(): FormControl {
+      return this.loginForm.get('email') as FormControl;
+  }
+
+  get password(): FormControl {
+      return this.loginForm.get('password') as FormControl;
   }
 
 }
--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
     imports: [
+        ReactiveFormsModule,
         MatInputModule,
         MatButtonModule,
     ],
