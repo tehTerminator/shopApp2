c1f91cd6dcee4019e11098322450657bc704f71d
Prateek Kher <prateek.kher@gmail.com>
2020-11-15 12:53:50 +0530

    Added Firebase Config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD-iu7KNNGeXD2jkk5BzbFXXtUkdnO5dwc",
+    authDomain: "maharajac-cb64a.firebaseapp.com",
+    databaseURL: "https://maharajac-cb64a.firebaseio.com",
+    projectId: "maharajac-cb64a",
+    storageBucket: "maharajac-cb64a.appspot.com",
+    messagingSenderId: "507251075517",
+    appId: "1:507251075517:web:a001c85a9d5246a3550ad4",
+    measurementId: "G-Q9V0KKJWDD"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD-iu7KNNGeXD2jkk5BzbFXXtUkdnO5dwc",
+    authDomain: "maharajac-cb64a.firebaseapp.com",
+    databaseURL: "https://maharajac-cb64a.firebaseio.com",
+    projectId: "maharajac-cb64a",
+    storageBucket: "maharajac-cb64a.appspot.com",
+    messagingSenderId: "507251075517",
+    appId: "1:507251075517:web:a001c85a9d5246a3550ad4",
+    measurementId: "G-Q9V0KKJWDD"
+  }
 };
 
 /*
