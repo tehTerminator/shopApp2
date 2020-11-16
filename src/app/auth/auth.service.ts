e60b4d905c4cae88037b62442cb15aac598f7809
Prateek Kher <prateek.kher@gmail.com>
2020-11-16 23:48:55 +0530

    Removed Firebase Code

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,27 +1,21 @@
 import { Injectable } from '@angular/core';
-import firebase from 'firebase';
 import { BehaviorSubject } from 'rxjs';
-import { FirebaseService } from '../shared/firebase.service';
 import { User } from './user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  user: BehaviorSubject<firebase.User> = new BehaviorSubject(null);
+  user: BehaviorSubject<User> = new BehaviorSubject(null);
 
-  constructor(private firebaseService: FirebaseService) { }
+  constructor() { }
 
   async signIn(email: string, password: string) {
-    this.firebaseService.auth.signInWithEmailAndPassword(email, password)
-    .catch((error: firebase.auth.Error) => {
-      throw error;
-    })
-    .then((userCredentials) => this.handleAuthentication(userCredentials))
+
   }
 
-  handleAuthentication(userCredentials: firebase.auth.UserCredential) {
-
+  private handleAuthentication() {
+    
   }
 }
 
--- src/app/auth/user.model.ts
+++ src/app/auth/user.model.ts
@@ -1,17 +1,20 @@
 export class User{
     constructor(
-        public email: string,
-        public userId: string,
-        private theToken: string,
-        private expiresIn: Date
+        public uid: string,
+        public username: string,
+        public displayName: string,
+        private _token: string,
+        private expirationTime: number
     ) {}
 
     get token(): string {
         const currentTime = (new Date()).getTime();
-        if(this.expiresIn.getTime() < currentTime) {
-            return this.theToken;
+        if(this.expirationTime > currentTime) {
+            return this._token;
         }
         return null;
     }
+
+
 }
 
