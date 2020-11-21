09777e2487d1498e2f549eab6cea332852950657
Prateek Kher <prateek.kher@gmail.com>
2020-11-21 12:00:21 +0530

    Added Workspace File

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
         ReactiveFormsModule,
         MatInputModule,
         MatButtonModule
-    ]
+    ],
+    providers: []
 })
 export class CoreModule{}
