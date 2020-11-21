f941f7ff13973995bc110555b81270db1aa8c0c9
Prateek Kher <prateek.kher@gmail.com>
2020-11-21 22:08:10 +0530

    [Initial Build] Can Add Token and Set Url

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,4 +1,5 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { AuthService } from './auth.service';
@@ -9,14 +10,16 @@ export class AuthInterceptor implements HttpInterceptor {
     constructor(private authService: AuthService) {}
     
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        this.authService.user.pipe(
-            take(1)
-        ).subscribe((user: User) => {
+        this.authService.user.pipe(take(1))
+        .subscribe((user: User) => {
             const token = user.token;
 
-            if (token !== null) {
-                const req = 
-            }
-        })
+            const newRequest = req.clone({
+                url: environment.baseUrl + req.url,
+                setHeaders: {'Authorization': token}
+            });
+
+            return next.handle(newRequest);
+        });
     }
 }
