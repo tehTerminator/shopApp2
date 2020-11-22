dbc7eec04859ed651c4da9a769fb9127e0572c80
Prateek Kher <prateek.kher@gmail.com>
2020-11-22 19:57:06 +0530

    Added @Injectable Decorator

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,9 +1,13 @@
+import { Inject, Injectable } from '@angular/core';
 import { CanActivate, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 import { User } from './user.model';
 
+@Injectable({
+    providedIn: 'root'
+})
 export class AuthGuard implements CanActivate {
     constructor(private authService: AuthService, private router: Router) { }
 
