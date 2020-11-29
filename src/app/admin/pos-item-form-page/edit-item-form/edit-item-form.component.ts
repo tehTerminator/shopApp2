e35db21568d92882f8f65c1ae0c49ae9ee7e1145
Prateek Kher <prateek.kher@gmail.com>
2020-11-29 23:31:53 +0530

    [Empty] Created Edit Item Form

---
+++ src/app/admin/pos-item-form-page/edit-item-form/edit-item-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { PosItemService } from '../../../shared/pos-item.service';
+
+@Component({
+  selector: 'app-edit-item-form',
+  templateUrl: './edit-item-form.component.html',
+  styleUrls: ['./edit-item-form.component.css']
+})
+export class EditItemFormComponent implements OnInit {
+  constructor(
+  ) { }
+
+  ngOnInit(): void {
+  }
+}
