a4990d3ae69e56bee8c9825cb3353cd814d2f24a
Prateek Kher <prateek.kher@gmail.com>
2020-11-29 23:29:35 +0530

    Added Pos-Item Route

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminComponent } from './admin.component';
+import { PosItemListPageComponent } from './pos-item-list-page/pos-item-list-page.component';
 
 const routes: Routes = [
   { path: '', component: AdminComponent, children: [
     { path: 'collection', loadChildren: () => import('./collection-page/collection-page.module').then(m => m.CollectionPageModule) },
+    { path: 'pos-item', loadChildren: () => import('./pos-item-form-page/pos-item-form-page.module').then(m => m.PosItemFormPageModule) }, 
     { path: '**', redirectTo: 'collection', pathMatch: 'full' }
-  ]}, 
+  ]},
 ];
 
 @NgModule({
