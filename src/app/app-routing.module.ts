60950b6aeff899a7c801048ed3553ff1a0e40d80
Prateek Kher <prateek.kher@gmail.com>
2020-11-29 23:28:19 +0530

    [Modified] Added Route Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { AuthGuard } from './auth/auth.guard';
 const routes: Routes = [
     { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
     { path: 'home', canActivate: [AuthGuard], loadChildren: () => import('./home-page/home-page.module').then(m => m.HomePageModule) },
-    { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
-    { path: '**', redirectTo: '/auth', pathMatch: 'full' }
+    { path: 'admin', canActivate: [AuthGuard], loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+    { path: '**', redirectTo: '/auth/sign-in', pathMatch: 'full' }
 ];
 
 @NgModule({
